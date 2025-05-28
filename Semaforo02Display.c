0823085d8ffe65765e32940654acb81448fadb99
Maic <maic1405@gmail.com>
2025-05-27 22:04:29 -0300

    CHORE: comentários agora evidenciam o fluxo de execução

--- Semaforo02Display.c
+++ Semaforo02Display.c
@@ -16,28 +16,30 @@
 #define ENDERECO 0x3C
 
 #define BOTAO_A 5 // Gera evento
-#define BOTAO_B 6 // BOOTSEL
+#define BOTAO_B 6 // Gera evento
 #define MAX_PERSONS 10 // Número máximo de pessoas
 
-SemaphoreHandle_t xSemaphorContadorIn;
-SemaphoreHandle_t xSemaphorContadorOut;
-SemaphoreHandle_t xSemaphorBinaryReset;
+SemaphoreHandle_t xSemaphorContadorIn; // Semáforo para entrada de pessoas
+SemaphoreHandle_t xSemaphorContadorOut; // Semáforo para saída de pessoas
+SemaphoreHandle_t xSemaphorBinaryReset; // Semáforo binário para reset do contador
 
 ssd1306_t ssd;
 SemaphoreHandle_t xMutexSem;
 
-int totalPersons = 0;
-char buffer[32];
+int totalPersons = 0; // Variável para controlar o número de pessoas ativas
+char buffer[32]; // Variável utilizada para armazenar strings temporárias
 int lastTime = 0; // Variável para armazenar o tempo do último evento
-bool buzzerState = false; // Estado do buzzer
+bool buzzerState = false; // Estado do buzzer para o limite mínimo de pessoas
 bool buzzerState1 = false; // Estado do buzzer para o limite máximo de pessoas
-bool maxLimitBeeped = false;
-bool zeroBeeped = false;
+bool maxLimitBeeped = false; // Variável para impedir que o buzzer toque repetidamente quando o limite máximo é atingido
+bool zeroBeeped = false; // Variável para impedir que o buzzer toque repetidamente quando o número de pessoas chega a zero
 
+ // Task de entrada
 void vInTask(void *params) {
     while (true) {
-        // Aguarda semáforo (um evento)
-        if(xSemaphoreTake(xSemaphorContadorIn, portMAX_DELAY) == pdTRUE) {
+        // Entra somente quando há uma task de entrada na fila e ao mesmo tempo o número de usuários é menor que o máximo permitido
+        if(xSemaphoreTake(xSemaphorContadorIn, portMAX_DELAY) == pdTRUE && totalPersons < MAX_PERSONS) {
+            // Tenta adquirir o mutex para garantir acesso exclusivo à variável totalPersons e display
             if (xSemaphoreTake(xMutexSem, portMAX_DELAY) == pdTRUE) {
                 totalPersons++;
                 ssd1306_fill(&ssd, false); // Limpa o display
@@ -45,19 +47,12 @@ void vInTask(void *params) {
                 ssd1306_line(&ssd, 3, 37, 123, 37, true);           // Desenha uma linha
                 ssd1306_draw_string(&ssd, "", 8, 6); // Desenha uma string
                 ssd1306_draw_string(&ssd, "", 20, 16);  // Desenha uma string
-                sprintf(buffer, "Usuarios: %d", totalPersons);
+                sprintf(buffer, "Usuarios: %d", totalPersons); // Formata a string com o número de usuários
                 ssd1306_draw_string(&ssd, buffer, 10, 18);  // Desenha uma string
                 sprintf(buffer, "Vagas: %d", (MAX_PERSONS-totalPersons));
                 ssd1306_draw_string(&ssd, buffer, 10, 45);    // Desenha uma string
-                ssd1306_send_data(&ssd);
+                ssd1306_send_data(&ssd); // Envia os dados para o display OLED
                 vTaskDelay(pdMS_TO_TICKS(1000)); // Aguarda 1 tick
-                if (totalPersons == MAX_PERSONS && !maxLimitBeeped) {
-                    buzzerState1 = true;
-                    maxLimitBeeped = true;
-                }
-                if (totalPersons < MAX_PERSONS) {
-                    maxLimitBeeped = false; // libera para tocar de novo futuramente
-                }
 
                 xSemaphoreGive(xMutexSem); // Libera o mutex para outras tarefas
             }
@@ -65,10 +60,12 @@ void vInTask(void *params) {
     }
 }
 
+// Task de saída
 void vOutTask(void *params) {
     while(true) {
-        // Aguarda semáforo (um evento)
-        if(xSemaphoreTake(xSemaphorContadorOut, portMAX_DELAY) == pdTRUE) {
+        // Sai somente quando há uma task de saída na fila e ao mesmo tempo o número de usuários é maior que zero
+        if(xSemaphoreTake(xSemaphorContadorOut, portMAX_DELAY) == pdTRUE && totalPersons > 0) {
+            // Tenta adquirir o mutex para garantir acesso exclusivo à variável totalPersons e display
             if(xSemaphoreTake(xMutexSem, portMAX_DELAY) == pdTRUE) {
                 totalPersons--;
                 ssd1306_fill(&ssd, false); // Limpa o display
@@ -76,20 +73,12 @@ void vOutTask(void *params) {
                 ssd1306_line(&ssd, 3, 37, 123, 37, true);           // Desenha uma linha
                 ssd1306_draw_string(&ssd, "", 8, 6); // Desenha uma string
                 ssd1306_draw_string(&ssd, "", 20, 16);  // Desenha uma string
-                sprintf(buffer, "Usuarios: %d", totalPersons);
+                sprintf(buffer, "Usuarios: %d", totalPersons); // Formata a string com o número de usuários
                 ssd1306_draw_string(&ssd, buffer, 10, 18);  // Desenha uma string
                 sprintf(buffer, "Vagas: %d", (MAX_PERSONS-totalPersons));
                 ssd1306_draw_string(&ssd, buffer, 10, 45);    // Desenha uma string
-                ssd1306_send_data(&ssd);
+                ssd1306_send_data(&ssd); // Envia os dados para o display OLED
                 vTaskDelay(pdMS_TO_TICKS(1000)); // Aguarda 1 tick
-
-                if (totalPersons == 0 && !zeroBeeped) {
-                    buzzerState = true;
-                    zeroBeeped = true;
-                }
-                if (totalPersons > 0) {
-                    zeroBeeped = false; // libera para tocar de novo futuramente
-                }
 
                 xSemaphoreGive(xMutexSem); // Libera o mutex para outras tarefas
             }
@@ -97,10 +86,12 @@ void vOutTask(void *params) {
     }
 }
 
+// Task de reset do contador
 void vResetTask(void *params) {
     while (true) {
+        // Aguarda o semáforo binário de reset ser liberado
         if(xSemaphoreTake(xSemaphorBinaryReset, portMAX_DELAY) == pdTRUE) {
-            // Zera os semáforos manualmente
+            // Zera os semáforos manualmente rodando e dando take enquanto existirem tarefas na fila
             while(xSemaphoreTake(xSemaphorContadorIn, 0) == pdTRUE);
             while(xSemaphoreTake(xSemaphorContadorOut, 0) == pdTRUE);
 
@@ -116,29 +107,45 @@ void vResetTask(void *params) {
             ssd1306_draw_string(&ssd, buffer, 10, 18);  // Desenha uma string
             sprintf(buffer, "Vagas: %d", (MAX_PERSONS-totalPersons));
             ssd1306_draw_string(&ssd, buffer, 10, 45);    // Desenha uma string
-            ssd1306_send_data(&ssd);   
+            ssd1306_send_data(&ssd); // Envia os dados para o display OLED
 
             printf("Reset executado com sucesso\n");
         }
     }
 }
 
+// Task que controla o estado do buzzer e verifica os limites de pessoas
+void vStateControllerTask(void *params) {
+    while(true) {
+        if (totalPersons < MAX_PERSONS) {
+            maxLimitBeeped = false; // libera para tocar de novo futuramente
+        }
+        if (totalPersons > 0) {
+            zeroBeeped = false; // libera para tocar de novo futuramente
+        }
+        if (totalPersons == MAX_PERSONS && !maxLimitBeeped) {
+            buzzerState1 = true; // Ativa o buzzer para o limite máximo de pessoas
+            maxLimitBeeped = true; // Impede que o buzzer toque repetidamente quando o limite máximo é atingido
+        }
+        if (totalPersons == 0 && !zeroBeeped) {
+            buzzerState = true; // Ativa o buzzer para o limite mínimo de pessoas
+            zeroBeeped = true; // Impede que o buzzer toque repetidamente quando o número de pessoas chega a zero
+        }
+        vTaskDelay(pdMS_TO_TICKS(10)); // Aguarda 10 ms antes de verificar novamente
+    }
+}
+
+// Função de interrupção para os botões
 void gpio_irq_handler(uint gpio, uint32_t events) {
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
     uint32_t currentTime = to_us_since_boot(get_absolute_time());
 
     if(currentTime - lastTime > 200000) { // Debounce
-        if (gpio == BOTAO_A) {
-            if(totalPersons<MAX_PERSONS) {
-                xSemaphoreGiveFromISR(xSemaphorContadorIn, &xHigherPriorityTaskWoken);
-            }
-            printf("%d\n", uxSemaphoreGetCount(xSemaphorContadorIn)); // Exibe a contagem do semáforo
-        } else if (gpio == BOTAO_B) {
-            if(totalPersons>0) {
-                xSemaphoreGiveFromISR(xSemaphorContadorOut, &xHigherPriorityTaskWoken);
-            }
-            printf("%d\n", uxSemaphoreGetCount(xSemaphorContadorOut)); // Exibe a contagem do semáforo
-        } else if(gpio == 22) {                  // Incializa com pdFALSE
+        if (gpio == BOTAO_A) { // Se o botão A for pressionado aumenta o contador
+            xSemaphoreGiveFromISR(xSemaphorContadorIn, &xHigherPriorityTaskWoken);
+        } else if (gpio == BOTAO_B) { // Se o botão B for pressionado diminui o contador
+            xSemaphoreGiveFromISR(xSemaphorContadorOut, &xHigherPriorityTaskWoken);
+        } else if(gpio == 22) { // Se o botão 22 for pressionado reseta o contador
             xSemaphoreGiveFromISR(xSemaphorBinaryReset, &xHigherPriorityTaskWoken); // Libera o semáforo de reset
         }
         lastTime = currentTime; // Atualiza o tempo do último evento
@@ -147,6 +154,7 @@ void gpio_irq_handler(uint gpio, uint32_t events) {
     portYIELD_FROM_ISR(xHigherPriorityTaskWoken); // Troca o contexto da tarefa se necessário
 }
 
+// Task que controla os LEDs de acordo com o número de pessoas
 void vLedTask() {
     while (true) {
         if(totalPersons == 0) {
@@ -154,22 +162,23 @@ void vLedTask() {
             gpio_put(12, true);
             gpio_put(13, false);
         } else if(totalPersons > 0 && totalPersons <= MAX_PERSONS-2) {
-            gpio_put(11, true); // Desliga o LED se não houver pessoas
-            gpio_put(12, false); // Desliga o LED azul
-            gpio_put(13, false); // Liga o LED vermelho se houver pessoas
+            gpio_put(11, true);
+            gpio_put(12, false);
+            gpio_put(13, false); 
         } else if(totalPersons == MAX_PERSONS-1) {
-            gpio_put(11, true); // Desliga o LED se não houver pessoas
-            gpio_put(12, false); // Desliga o LED azul
-            gpio_put(13, true); // Liga o LED se houver pessoas
+            gpio_put(11, true);
+            gpio_put(12, false);
+            gpio_put(13, true);
         } else {
-            gpio_put(11, false); // Desliga o LED verde se houver pessoas
-            gpio_put(12, false); // Desliga o LED azul se houver pessoas
-            gpio_put(13, true); // Liga o LED se houver pessoas
+            gpio_put(11, false);
+            gpio_put(12, false);
+            gpio_put(13, true);
         }
-        vTaskDelay(1); // Aguarda 100 ms antes de verificar novamente
+        vTaskDelay(1); // Aguarda 1 tick antes de verificar novamente
     }
 }
 
+// Função para tocar o buzzer
 void tocar_buzzer(uint slice, uint wrap, int repeticoes, int duracao_ms, int pausa_ms) {
     for (int i = 0; i < repeticoes; i++) {
         gpio_set_function(21, GPIO_FUNC_PWM);
@@ -186,6 +195,7 @@ void tocar_buzzer(uint slice, uint wrap, int repeticoes, int duracao_ms, int pau
     }
 }
 
+// Task que controla o buzzer
 void vBuzzerTask(void *params) {
     const uint slice_num = pwm_gpio_to_slice_num(21);
     const uint32_t clock = clock_get_hz(clk_sys);
@@ -199,20 +209,20 @@ void vBuzzerTask(void *params) {
     pwm_init(slice_num, &config, false);
 
     while (true) {
+        // Verifica o estado dos buzzers e toca se necessário
         if (buzzerState) {
             tocar_buzzer(slice_num, wrap, 2, 50, 50);
-            buzzerState = false;
+            buzzerState = false; // Reseta o estado do buzzer para não tocar infinitamente
         }
-
         if (buzzerState1) {
             tocar_buzzer(slice_num, wrap, 1, 50, 0);
-            buzzerState1 = false;
+            buzzerState1 = false; // Reseta o estado do buzzer para não tocar infinitamente
         }
-
         vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
 
+// Função de inicialização dos LEDs
 void gpioInit(int led) {
     gpio_init(led);
     gpio_set_dir(led, GPIO_OUT);
@@ -253,18 +263,16 @@ int main() {
     gpio_set_irq_enabled(BOTAO_B, GPIO_IRQ_EDGE_FALL, true);
     gpio_set_irq_enabled(22     , GPIO_IRQ_EDGE_FALL, true);
 
-    // Cria semáforo de contagem (máximo 10, inicial 0)
-    xSemaphorContadorIn = xSemaphoreCreateCounting(10, 0);
-    xSemaphorContadorOut = xSemaphoreCreateCounting(10, 0);
-    xSemaphorBinaryReset = xSemaphoreCreateBinary();
-    xMutexSem = xSemaphoreCreateMutex();
+    xSemaphorContadorIn = xSemaphoreCreateCounting(10, 0); // Semáforo para entrada de pessoas
+    xSemaphorContadorOut = xSemaphoreCreateCounting(10, 0); // Semáforo para saída de pessoas
+    xSemaphorBinaryReset = xSemaphoreCreateBinary(); // Semáforo binário para reset do contador
+    xMutexSem = xSemaphoreCreateMutex(); // Mutex para proteger o acesso à variável totalPersons e ao display
 
-    bool cor = true;
-
-    ssd1306_fill(&ssd, !cor);                          // Limpa o display
-
-    ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);      // Desenha um retângulo
-    ssd1306_line(&ssd, 3, 37, 123, 37, cor);           // Desenha uma linha
+    // Iniciliza o display pela primeira vez, independente de qualquer evento
+    // Esse trecho existe pra que o displlay não comece desligado
+    ssd1306_fill(&ssd, false);                          // Limpa o display
+    ssd1306_rect(&ssd, 3, 3, 122, 60, true, false);      // Desenha um retângulo
+    ssd1306_line(&ssd, 3, 37, 123, 37, true);           // Desenha uma linha
     ssd1306_draw_string(&ssd, "", 8, 6); // Desenha uma string
     ssd1306_draw_string(&ssd, "", 20, 16);  // Desenha uma string
     ssd1306_draw_string(&ssd, "Usuarios: 0", 10, 18);  // Desenha uma string
@@ -277,6 +285,7 @@ int main() {
     xTaskCreate(vResetTask, "ResetTask", configMINIMAL_STACK_SIZE + 128, NULL, 1, NULL);
     xTaskCreate(vLedTask, "LedTask", configMINIMAL_STACK_SIZE + 128, NULL, 1, NULL);
     xTaskCreate(vBuzzerTask, "BuzzerTask", configMINIMAL_STACK_SIZE + 128, NULL, 1, NULL);
+    xTaskCreate(vStateControllerTask, "Task", configMINIMAL_STACK_SIZE + 128, NULL, 1, NULL);
 
     vTaskStartScheduler();
     panic_unsupported();
